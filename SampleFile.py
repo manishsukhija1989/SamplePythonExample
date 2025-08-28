82a6a3e58f6dee3a86538027368a0d0bf5670c38
manishsukhija1989 <manishsukhija1989@gmail.com>
2025-08-28 23:05:18 +0530

    main

--- SampleFile.py
+++ SampleFile.py
@@ -13,4 +13,4 @@ def hello():
 
 # Run Uvicorn server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
