c515b4886f095bbab81279a7f6270ee06789f126
manishsukhija1989 <manishsukhija1989@gmail.com>
2025-08-29 17:41:43 +0530

    Approach changed

--- SampleFile.py
+++ SampleFile.py
@@ -1,16 +1,32 @@
-import uvicorn
-from fastapi import FastAPI
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+
+# creating a Flask app
+app = Flask(__name__)
+
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+
+        data = "hello world"
+        return jsonify({'data': data})
 
 
-# FastAPI app instance
-app = FastAPI()
+# A simple function to calculate the square of a number
+# the number to be squared is sent in the URL when we use GET
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# this returns 100 (square of 10)
+@app.route('/home/<int:num>', methods = ['GET'])
+def disp(num):
+
+    return jsonify({'data': num**2})
 
 
-@app.get("/")
-def hello():
-    return {"Hello": "World"}
+# driver function
+if __name__ == '__main__':
 
-
-# Run Uvicorn server
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
+    app.run(debug = True)
